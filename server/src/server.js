ddb73e8bb23363d87de5d2889287300d527516cf
OmerAhmedQuadri <omerquadri953@gmail.com>
2026-05-29 20:18:48 +0530

    login fix

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,10 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
+
 import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
